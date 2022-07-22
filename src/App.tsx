27d837b17598c980eb1ff7e07b56712eee289112
cjustinn <such.justinn@gmail.com>
2022-07-22 17:15:57 -0400

    Created the listing page and set up to pull from ./public/mods.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function ArrayIncludesValue(_arr: any[], _property: string, _value: any) {
+  return (_arr.filter(e => e[_property] === _value).length > 0);
+}
+
+function GetElementIndexFromValue(_arr: any[], _property: string, _value: any) {
+  return _arr.findIndex(e => e[_property] === _value);
+}
+
 function App() {
+  const [ mods, setMods ] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("mods.json").then(_mods => {
+      return _mods.json();
+    }).then(data => {
+      let organizedMods: any[] = [];
+  
+      for (let i = 0; i < data.mods.length; i++) {
+        if (ArrayIncludesValue(organizedMods, `type`, data.mods[i].type)) {
+          const idx = GetElementIndexFromValue(organizedMods, `type`, data.mods[i].type);
+          organizedMods[idx].mods.push(data.mods[i]);
+        } else {
+
+          organizedMods.push({
+            type: data.mods[i].type,
+            mods: [ data.mods[i] ]
+          });
+
+        }
+      }
+  
+      setMods(organizedMods);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-background">
+      <h1 className="text-white font-bold text-5xl">Super Cool Minecraft Mod List</h1>
+      <h3 className="text-gray-300 font-thin text-xl">Click any card to open the mod's CurseForge page</h3>
+
+      <p className="text-gray-100 font-thin text-md my-6 w-1/2 text-center">
+        I'm sure you'll be able to figure it out, but any mod that is marked as required is going to be server-side and client-side, so to join the server you'll <i>have</i> to have it installed.
+        Anything that is <b>NOT</b> marked required is optional, and you can add them (or add any others) as you see fit since they're purely client-side.
+      </p>
+      
+      <div className="text-start w-full">
+        {
+          mods.sort((a: any, b: any) => a.type.localeCompare(b.type)).map((category: any) => {
+            return (
+              <div key={category.type}>
+                <p className="text-white text-2xl font-bold mb-2">{category.type}</p>
+
+                <div className="grid gap-5 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 grid-rows-auto w-full mb-12">
+                  {
+                    category.mods.sort((a: any, b:any) => a.name.localeCompare(b.name)).sort((a: any, b:any) => b.required - a.required).map((mod: any) => {
+                      return (
+                        <a key={mod.id} className="card shadow-red-300" href={mod.link} target="_blank">
+                          <div className="card-body">
+                            <div className="w-full">
+                              <p className="float-left text-white font-bold text-lg">{mod.name}</p>
+                              {
+                                mod.required ?
+                                <p className="float-right text-red-300 text-xs">REQUIRED</p>
+                                :
+                                null
+                              }
+                            </div>
+                            
+                            <p className="float-left text-gray-300 text-md">{mod.type}</p>
+                          </div>
+                        </a>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
